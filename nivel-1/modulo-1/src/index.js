aa1d935d8ea910739e3e788943f62b5716f032fd
Gabriel Guimaraes <gabriel.ctiunesp@gmail.com>
2020-10-28 00:15:20 -0300

    Add cors

--- nivel-1/modulo-1/src/index.js
+++ nivel-1/modulo-1/src/index.js
@@ -1,9 +1,10 @@
 const uuidv4 = require("uuidv4");
-
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 const projects = [];
